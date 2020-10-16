2937923166bd0674a2cfb3d7c7909543b871a3a3
DubKoldun <epifantsev.vitaliy@gmail.com>
2020-10-16 23:36:59 +0300

    🚑 Upd vscode settings

--- nixpkgs/development/vscode/settings.nix
+++ nixpkgs/development/vscode/settings.nix
@@ -2,7 +2,7 @@
 
 with config.lib; {
  
- userSettings = {
+  userSettings = {
             "omnisharp.enableRoslynAnalyzers" = true;
             "omnisharp.path" = "/home/vutaliy/.nix-profile/bin/omnisharp";
             "workbench.colorTheme" = "Monokai Dimmed";
@@ -13,18 +13,19 @@ with config.lib; {
             "debug.javascript.usePreview" = true;
             "gitmoji.outputType" = "code";
             "editor.tabSize" = 2;
-            "github.gitAuthentication" = false;
       };
 
-      "ghcSimple.startupCommands.all" = [
-        "System.IO.hSetBuffering System.IO.stderr System.IO.NoBuffering"
-        "System.IO.hSetBuffering System.IO.stdout System.IO.NoBuffering"
-        ":set -fno-diagnostics-show-caret -fdiagnostics-color=never -ferror-spans"
-        ":set -fdefer-type-errors -fdefer-typed-holes -fdefer-out-of-scope-variables"
-        ":seti -fno-defer-type-errors -fno-defer-typed-holes -fno-defer-out-of-scope-variables"
-        ":set -haddock"
-        ":set -fno-hide-source-paths"
-        ":set -Wno-error=missing-home-modules"
+  "github.gitAuthentication" = false;
+  
+  "ghcSimple.startupCommands.all" = [
+    "System.IO.hSetBuffering System.IO.stderr System.IO.NoBuffering"
+    "System.IO.hSetBuffering System.IO.stdout System.IO.NoBuffering"
+    ":set -fno-diagnostics-show-caret -fdiagnostics-color=never -ferror-spans"
+    ":set -fdefer-type-errors -fdefer-typed-holes -fdefer-out-of-scope-variables"
+    ":seti -fno-defer-type-errors -fno-defer-typed-holes -fno-defer-out-of-scope-variables"
+    ":set -haddock"
+    ":set -fno-hide-source-paths"
+    ":set -Wno-error=missing-home-modules"
     ];
 
 }
