41fde5798afe76949879f5dbdb903bbb34abea67
DubKoldun <epifantsev.vitaliy@gmail.com>
2021-01-10 17:08:10 +0300

    🔧 smol changes in config giles

--- nixpkgs/window-manager/default.nix
+++ nixpkgs/window-manager/default.nix
@@ -2,5 +2,9 @@
 
 {
     xdg.configFile."awesome/rc.lua".source = ./config.lua;
-    xdg.configFile."awesome/theme.lua".source = ./theme.lua;
+    xdg.configFile."awesome/theme.lua".source = ./theme.lua; # default theme
+    # xdg.configFile."awesome/theme.lua".source = ./powerarrow/theme.lua;
+    # xdg.configFile."awesome/icons".source = ./powerarrow/icons;
+    # xdg.configFile."awesome/binblock.lua".source = ./powerarrow/binclock.lua;
+    # xdg.configFile."awesome/wall.png".source = ./powerarrow/wall.png;
 }
